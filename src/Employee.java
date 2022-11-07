7fe4738650a0aa22f4d94abab33f72686c2b4f81
kchzhao <kamila.chzhao@gmail.com>
2022-11-07 04:13:43 -0500

    commit to branch

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,56 @@
 public class Employee {
+    private final String fullName;
+    private int department;
+    private int salary;
+    private int iD;
+    private static int counter = 0;
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.iD = counter++;
+    }
+    //Getters
+    public String getFullName(){
+        return fullName;
+    }
+
+    public int getDepartment(){
+        return department;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public int getiD(){
+        return iD;
+    }
+
+    public static int getCounter(){
+        return counter;
+    }
+    //Setters
+    public void setDepartment(int department){
+        this.department = department;
+    }
+
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+
+    //toString
+    @Override
+    public String toString(){
+        String employeeName = "Employee name: " + fullName + ", ";
+        String employeeDepartment = "Department: " + department + ", ";
+        String employeeSalary = "Salary: " + salary + ", ";
+        String employeeID = "ID: " + iD;
+        return employeeName + employeeDepartment + employeeSalary + employeeID;
+    }
+
+
+
+
 }
